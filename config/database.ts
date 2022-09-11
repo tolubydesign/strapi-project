726d7a42439beb1f15d4863c9ac4370c24f53fb4
Tolu Adesina <tolu.adesina79@gmail.com>
2022-09-11 23:36:46 +0200

    REVERT i made an oopsie

--- config/database.ts
+++ config/database.ts
@@ -1,38 +1,98 @@
+// DATABASE CHANGES MADE HERE
 import path from 'path';
+import { Knex, knex } from 'knex';
 
 export default ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
+    connector: 'bookshelf',
     connection: {
-      filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
+    pool: { min: 0 },
     useNullAsDefault: true,
+    debug: false,
   },
 });
 
+// KNEX CONFIGURATION TYPE
+// Keep getting 'Error: Unknown dialect undefined _-_ at getDialectClass (/home/tolua/hearx/strapi-project/node_modules/@strapi/database/lib/dialects/index.js:12:13)'
+// export default ({ env }): Knex.Config => ({
+//   client: 'pg',
+//   version: '7.2',
+//   dialect: "postgres",
+//   connection: {
+//     // client: "postgres",
+//     host: env("DATABASE_HOST", "127.0.0.1"),
+//     port: env.int("DATABASE_PORT", 5432),
+//     database: env("DATABASE_NAME", "strapi"),
+//     user: env("DATABASE_USERNAME", ""),
+//     password: env("DATABASE_PASSWORD", ""),
+//     // schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//     ssl: {
+//       rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//     },
+//     pool: { min: 0, max: 7 },
+//     debug: false,
+//   },
+//   useNullAsDefault: true,
+// })
+
+// Previous version
+// export default ({ env }) => ({
+//   connection: {
+//     client: "postgres",
+//     connection: {
+//       // host: env("DATABASE_HOST", "127.0.0.1"),
+//       port: env.int("DATABASE_PORT", 5432),
+//       database: env("DATABASE_NAME", "strapi"),
+//       user: env("DATABASE_USERNAME", ""),
+//       password: env("DATABASE_PASSWORD", ""),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
+//     },
+//     pool: { min: 0, max: 7 },
+//     useNullAsDefault: true,
+//     debug: false,
+//   },
+// });
+
+// DELETE: EDITED
 // module.exports = ({ env }) => ({
 //   connection: {
-//     client: ‘postgres’,
+//     client: 'postgres',
 //     connection: {
-//       host: env(‘DATABASE_HOST’, ‘127.0.0.1’),
-//       port: env.int(‘DATABASE_PORT’, 5432),
-//       database: env(‘DATABASE_NAME’, ‘strapi’),
-//       user: env(‘DATABASE_USERNAME’, ‘strapi’),
-//       password: env(‘DATABASE_PASSWORD’, ‘strapi’),
-//       schema: env(‘DATABASE_SCHEMA’, ‘public’), // Not required
-//       ssl: env(‘DATABASE_SSL’, false)
-//     },
-//     acquireConnectionTimeout: 1000000,
-//     pool: {
-//       min: 0,
-//       max: 1,
-//       acquireTimeoutMillis: 300000,
-//       createTimeoutMillis: 300000,
-//       destroyTimeoutMillis: 300000,
-//       idleTimeoutMillis: 30000,
-//       reapIntervalMillis: 1000,
-//       createRetryIntervalMillis: 2000
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'strapi'),
+//       user: env('DATABASE_USERNAME', 'strapi'),
+//       password: env('DATABASE_PASSWORD', 'strapi'),
+//       schema: env('DATABASE_SCHEMA', 'public'), // Not required
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
 //     },
 //     debug: false,
+//   },
+// });
+
+// Original Code.
+// export default ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
 //   },
 // });
---
+++ config/plugins.ts
@@ -0,0 +1,16 @@
+// DATABASE CHANGES MADE HERE
+export default ({ env }) => ({
+  upload: {
+      config: {
+          provider: 'aws-s3',
+          providerOptions: {
+              accessKeyId: env('AWS_ACCESS_KEY_ID'),
+              secretAccessKey: env('AWS_ACCESS_SECRET'),
+              region: env('AWS_REGION'),
+              params: {
+                  Bucket: env('AWS_BUCKET_NAME'),
+              },
+          },
+      },
+  }
+});
