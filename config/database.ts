3d3f76e20ada3485566ecb6216eadf723e93800d
Tolu Adesina <tolu.adesina79@gmail.com>
2022-10-03 20:48:54 +0200

    UPDATE database and strapi version

--- config/database.ts
+++ config/database.ts
@@ -1,34 +1,36 @@
 // DATABASE CHANGES MADE HERE
-import path from 'path';
 import { Knex, knex } from 'knex';
 
-export default ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connector: 'bookshelf',
+export default ({ env }) => {
+  return ({
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", ""),
-      password: env("DATABASE_PASSWORD", ""),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: env('DATABASE_SSL', false),
-      // ssl: {
-      //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-      // },
+      client: "postgres",
+      connector: 'bookshelf',
+      connection: {
+        host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        user: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+        password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not required
+        ssl: env('DATABASE_SSL', false),
+        // ssl: {
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+        // },
+      },
+      pool: {
+        min: 0,
+        max: 10,
+        acquireTimeoutMillis: 120000,
+        createTimeoutMillis: 60000,
+        idleTimeoutMillis: 60000,
+        propagateCreateError: false,
+      },
+      useNullAsDefault: true,
+      debug: false,
     },
-    pool: {
-      min: 0,
-      max: 10,
-      idleTimeoutMillis: 30000,
-      createTimeoutMillis: 30000,
-      acquireTimeoutMillis: 30000
-    },
-    useNullAsDefault: true,
-    debug: false,
-  },
-});
+  });
+}
 
 // KNEX CONFIGURATION TYPE
 // Keep getting 'Error: Unknown dialect undefined _-_ at getDialectClass (/home/tolua/hearx/strapi-project/node_modules/@strapi/database/lib/dialects/index.js:12:13)'
